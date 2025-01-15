76fd8ce1025fbfa629726d0dfdf7232726d1caa0
RanaIsHere <60571003+RanaIsHere@users.noreply.github.com>
2025-01-15 16:28:29 +0700

    Reworded the About me description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,11 +7,11 @@ function About() {
 
             <div className="section-content">
                 <div className="details">
-                    <p>My name is Rana Rosihan, an undergraduate Computer Science/Informatika student at Universitas Siber Muhammadiyah in Indonesia. With an optimistic outlook and a passion for technology as well as development from a young age, I am quite interested in all manners of topics such as game development, web development, and software development.</p>
+                    <p>My name is <b>Rana Rosihan</b>, an undergraduate Computer Science/Informatika student at Universitas Siber Muhammadiyah in Indonesia. With a passionate outlook for technology and development from a young age, I am quite interested in all manners of topics related to <b>game development</b>, <b>web development</b>, and <b>software development</b>.</p>
 
-                    <p>Though it does not end here! Aside from interest, I have accumulated understanding in the topics of web development from PHP to Javascript and frameworks such as <span className='highlight-react'>React</span> and <span className='highlight-laravel'>Laravel</span>. I am also quite versed in game development, such as Unity Engine and Godot Engine. Naturally, I'm quite interested in learning further more of the scopes within web development and game development.</p>
+                    <p>Aside from interest, I've accumulated practical skills and knowledge in the topics of web development, working with techs such as <span className='highlight-react'>React</span> and <span className='highlight-laravel'>Laravel</span>. I have also gathered enough knowledge in game development, such as Unity Engine and Godot Engine. Naturally, I'm quite eager to deepen my knowledge and learning further more of the scopes within web development and game development.</p>
 
-                    <p>At the moment, I'm currently looking for an opportunity to apply to either a junior position or an internship in web development or game development as I do not have much work experience. <b>However, due to <i>conditions</i>, I cannot accept an on-site or hybrid position.</b></p>
+                    <p>At the moment, I'm actively seeking for an opportunity to apply to either a junior position or an internship in web development or game development. However, due to certain <i>constraints</i> and <i>conditions</i>, I am only available for <b>remote positions</b> at this time.</p>
 
                     <div className="actions">
                         <a href="#projects">
